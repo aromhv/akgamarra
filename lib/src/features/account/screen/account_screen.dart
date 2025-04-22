756e516a0cc6ed8f2836bd286284ce908e00c1af
aromhv <68717901+aromhv@users.noreply.github.com>
2025-04-21 21:40:35 -0500

    cambios de estilos en el perfil

--- lib/src/features/account/screen/account_screen.dart
+++ lib/src/features/account/screen/account_screen.dart
@@ -1,7 +1,8 @@
-import 'package:akgamarra_app/src/core/context/auth_context.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+import 'package:akgamarra_app/src/core/context/auth_context.dart';
+import 'package:flutter/services.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({super.key});
@@ -9,43 +10,178 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final storeHandler = context.read<AuthContext>();
-
     final user = storeHandler.user;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (user != null && user.verified) ...[
-              Text(
-                "Nombre: ${user.firstName}",
-                style: const TextStyle(fontSize: 16),
+      body: Column(
+        children: [
+          // Header con fondo degradado
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 220,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFDA22FF), Color(0xFF9733EE)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(30),
+                  ),
+                ),
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 30),
+                    Text(
+                      user != null && user.verified
+                          ? '${user.firstName} ${user.lastName}'
+                          : user?.alias ?? '',
+                      style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      user != null && user.verified
+                          ? 'Bienvenido a tu perfil'
+                          : user?.email ?? '',
+                      style: const TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
               ),
-              Text(
-                "Apellido: ${user.lastName}",
-                style: const TextStyle(fontSize: 16),
+              Positioned(
+                bottom: -40,
+                left: 0,
+                right: 0,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: user?.picture != null && user!.picture.isNotEmpty
+                        ? NetworkImage(user.picture)
+                        : const AssetImage('assets/images/profile_placeholder.png') as ImageProvider,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
               ),
+
             ],
-            if (user != null && !user.verified) ...[
-              Text(
-                "Alias: ${user.alias}",
-                style: const TextStyle(fontSize: 16),
-              ),
-              Text(
-                "Correo: ${user.email}",
-                style: const TextStyle(fontSize: 16),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  context.go('/complete-data');
-                },
-                child: Text("Completar mis datos"),
-              ),
-            ],
-          ],
-        ),
+          ),
+          const SizedBox(height: 50),
+          // Stats (puedes reemplazarlos con datos reales si los tienes)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                _StatItem(label: 'Puntos', value: '6'),
+                _StatItem(label: 'Compras', value: '18'),
+                _StatItem(label: 'Pedidos', value: '20'),
+              ],
+            ),
+          ),
+          const Divider(height: 20),
+          // Opciones
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              children: [
+                // Mostrar los datos del usuario
+                if (user != null) ...[
+                  ListTile(
+                    title: Text('Nombre'),
+                    subtitle: Text(
+                      user != null && user.verified
+                          ? '${user.firstName ?? "Nombre no disponible"} ${user.lastName ?? "Apellido no disponible"}'
+                          : user?.alias ?? '',
+                      style: const TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Teléfono'),
+                    subtitle: Text(user.numberPhone ?? 'No disponible'
+
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Tipo de documento'),
+                    subtitle: Text(user.documentType ?? 'No disponible'),
+                  ),
+                  ListTile(
+                    title: Text('Número de documento'),
+                    subtitle: Text(user.documentNumber ?? 'No disponible'),
+                  ),
+                  if (user.storeId != null)
+                    ListTile(
+                      title: Text('ID de tienda'),
+                      subtitle: Text(user.storeId ?? 'No disponible'),
+                    ),
+                ],
+                // Agregar botón si el usuario no está verificado
+                if (user != null && !user.verified)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child:ElevatedButton(
+                      onPressed: () => context.go('/complete-data'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple, // Color de fondo
+                        foregroundColor: Colors.white, // Color del texto
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Espaciado interno
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30), // Bordes redondeados
+                        ),
+                        elevation: 5, // Sombra del botón
+                      ),
+                      child: const Text(
+                        "Completar mis datos",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // Estilo del texto
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          )
+
+        ],
       ),
+    );
+  }
+}
+
+class _StatItem extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const _StatItem({required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(value, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+        const SizedBox(height: 4),
+        Text(label, style: const TextStyle(color: Colors.grey)),
+      ],
+    );
+  }
+}
+
+class _OptionTile extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const _OptionTile({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(label),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+      onTap: () {},
     );
   }
 }
